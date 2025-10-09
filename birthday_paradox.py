64858146c0d8df9eff092290a215cc0909f64ade
bartk6 <bartholomew@bwalters.au>
2025-10-09 17:01:03 +1100

    bagels & birthday paradox

---
+++ birthday_paradox.py
@@ -0,0 +1,17 @@
+# in a group of 23 people there's a 50% chance of a matching birthday
+# list of dates?
+# each month has 30,31 except feb=28 once every 4 years 29 if leap year
+# leap year = divisible by 4 and years at end of century unless div by 400
+
+
+time_dict = [
+    "jan": 31,
+    "feb": 28,
+]
+
+
+
+
+birthdays = input("How many birthdays shall I generate?")
+
+
